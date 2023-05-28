eaff9061c1d265d430346bf5076dfb043dbd46a2
Vijay Swami <vijay.swami@autodesk.com>
2023-05-28 22:40:31 +0530

    component did update

--- src/Constructor.js
+++ src/Constructor.js
@@ -5,8 +5,27 @@ class Constructor extends Component {
     super(props);
     this.state = {
       count: 0,
+      favoriteFood: "rice",
     };
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    console.log("component did mount");
+    setTimeout(() => {
+      this.setState({ favoriteFood: "pizza" });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      console.log("Count has been updated:", this.state.count);
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log("componet should update");
+    return this.state.favoriteFood !== nextState.favoriteFood;
   }
 
   handleClick() {
@@ -14,11 +33,18 @@ class Constructor extends Component {
       count: prevState.count + 1,
     }));
   }
+
+  changeFood() {
+    this.setState({ favoriteFood: "puranpoli" });
+  }
   render() {
     return (
       <div>
         <p>Count: {this.state.count}</p>
         <button onClick={this.handleClick}>Increment</button>
+        <hr></hr>
+        <p>My favourite food is {this.state.favoriteFood}</p>
+        <button onClick={() => this.changeFood()}>Change food</button>
       </div>
     );
   }
